93da78f3fe95d3eb7368cf7f5ab4d99b73d5545f
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-04-24 11:37:32 +0300

    Update QuestionViewController with Questions and Answers data

--- PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/QuestionViewController.swift
@@ -10,33 +10,52 @@ import UIKit
 
 class QuestionViewController: UIViewController {
     
-// MARK: Properties:
+    // MARK: Properties:
+    
+    var questions: [Question] = [
+        Question(text:"Which food do you like the most?",
+                 type:.single,
+                 answers: [
+                    Answer(text: "Steak", type: .dog),
+                    Answer(text: "Fish", type: .cat),
+                    Answer(text: "Carrots", type: .rabbit),
+                    Answer(text: "Corn", type: .turtle)
+        ]),
+        
+        Question(text:"Which activities do you enjoy?",
+                 type:.multiple,
+                 answers: [
+                    Answer(text: "Swimming", type: .turtle),
+                    Answer(text: "Sleeping", type: .cat),
+                    Answer(text: "Cuddling", type: .rabbit),
+                    Answer(text: "Eating", type: .dog)
+        ]),
+        
+        Question(text:"How much do you enjoy car rides?",
+                 type:.ranged,
+                 answers: [
+                    Answer(text: "I dislike them", type: .cat),
+                    Answer(text: "I get a little nervous", type: .rabbit),
+                    Answer(text: "I barely notice them", type: .turtle),
+                    Answer(text: "I love them", type: .dog)
+        ])
+    ]
     
     
-// MARK: Start Here;:
+    // MARK: Start Here;:
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
     }
     
-// MARK: Methods:
+    // MARK: Methods:
     
     
-// MARK: Outlets:
+    // MARK: Outlets:
     
     
-// MARK: Actions:
+    // MARK: Actions:
     
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
