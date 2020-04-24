96fb0b65d27ba98eca5e2015ddc4d64b8f117490
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-04-24 16:41:14 +0300

    Update Actions for SingleStackView Buttons in QuestionViewController

--- PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/QuestionViewController.swift
@@ -122,6 +122,19 @@ class QuestionViewController: UIViewController {
     // MARK: Actions:
     
     @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+        
+        switch sender {
+        case singleButton1:
+            answersChosen.append(currentAnswers[0])
+        case singleButton2:
+            answersChosen.append(currentAnswers[1])
+        case singleButton3:
+            answersChosen.append(currentAnswers[2])
+        case singleButton4:
+            answersChosen.append(currentAnswers[3])
+        default:
+            break
+        }
     }
-    
 }
