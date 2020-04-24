02df0bcce7c3ce063508b1cad503ea9f51bfd0bb
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-04-24 06:49:49 +0300

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Office-iMac on 2020-04-24.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
