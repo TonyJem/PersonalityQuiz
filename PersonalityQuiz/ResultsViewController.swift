0e93b91b91b932316add0ec01cc3abe8ee22a4ca
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-04-24 19:16:41 +0300

    Initial update of ResultsViewController.swift file.

--- PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/ResultsViewController.swift
@@ -9,22 +9,21 @@
 import UIKit
 
 class ResultsViewController: UIViewController {
-
+    // MARK: Properties:
+    
+    // MARK: Start Here:
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    // MARK: Methods:
+    
+    
+    // MARK: Outlets:
+    
+    
+    // MARK: Actions:
+    
 }
