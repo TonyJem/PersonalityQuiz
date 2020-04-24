5c5dc3cf0e76b2d18952d3c6d13a111fb13ddf23
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-04-24 07:08:53 +0300

    Add Middle and Last VieControllers

---
+++ PersonalityQuiz/LastViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LastViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Office-iMac on 2020-04-24.
+//  Copyright © 2020 Tony Jem. All rights reserved.
+//
+
+import UIKit
+
+class LastViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
