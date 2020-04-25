fcd8500a6a1b407fa33011b8f01ee58351e56559
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-04-25 07:28:42 +0300

    Update "viewDidLoad" override func in ResultsViewController
    Add "hidesBackButton" for "navigationItem" to hide Back button on top left coner of ResultsViewController screen. It's prevents user against going from ResultsViewController screen back to QuestionViewController screen.

--- PersonalityQuiz/ResultsViewController.swift
+++ PersonalityQuiz/ResultsViewController.swift
@@ -16,6 +16,7 @@ class ResultsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        navigationItem.hidesBackButton = true
         calculatePersonalityResult()
     }
     
