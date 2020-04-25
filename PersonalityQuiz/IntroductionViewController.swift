41ce4f71b8444174277b5bdfceed4fd6f682335e
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-04-25 07:38:00 +0300

    Create @IBAction in IntroductionViewController
    Connect "Done" button in ResultsViewController with action to show IntroductionViewController screen after Done button was tapped

--- PersonalityQuiz/IntroductionViewController.swift
+++ PersonalityQuiz/IntroductionViewController.swift
@@ -15,6 +15,7 @@ class IntroductionViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func unwindToQuizIntoduction(segue: UIStoryboardSegue){
+    }
 }
 
